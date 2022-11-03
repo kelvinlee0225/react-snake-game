ba91c957fe09494596eb0ffa6b7da1a1873cf993
kelvinlee0225 <leekelvin0225@gmail.com>
2022-11-03 18:56:28 -0400

    delete unnecessary files

---
+++ src/app-style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const GameArena = styled.div`
+  position: relative;
+  width: 600px;
+  height: 600px;
+  border: 2px solid #000;
+  align-self: center;
+`;
