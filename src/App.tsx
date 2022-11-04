a31eb3c9e53511b5ee38e4ea4aadd20b50399430
kelvinlee0225 <leekelvin0225@gmail.com>
2022-11-04 16:58:26 -0400

    implement SnakeBody and Food components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import { GameArena, Container } from "./app-style";
+import { SnakeBody, Food } from "./components";
 
 function App() {
+  const [food, setFood] = useState([5, 7]);
+
+  const [dots, setDots] = useState([
+    [0, 0],
+    [0, 2],
+  ]);
+
   return (
     <Container>
-      <GameArena></GameArena>
+      <GameArena>
+        <SnakeBody dots={dots} />
+        <Food foodPosition={food} />
+      </GameArena>
     </Container>
   );
 }
--- src/app-style.ts
+++ src/app-style.ts
@@ -1,17 +1,17 @@
 import styled from "styled-components";
 
-export const GameArena = styled.div`
-  position: relative;
-  width: 600px;
-  height: 600px;
-  border: 2px solid #000;
-  align-self: center;
-`;
-
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
   align-items: center;
   display: flex;
   justify-content: center;
+`;
+
+export const GameArena = styled.div`
+  position: relative;
+  width: 600px;
+  height: 600px;
+  border: 2px solid #000;
+  align-self: center;
 `;
