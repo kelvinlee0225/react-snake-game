f969584a19a40d171fc5c2a578158791ddf9589d
kelvinlee0225 <leekelvin0225@gmail.com>
2022-11-09 18:46:50 -0400

    modify getRandomCooridnates method to generate random even numbers

--- src/utils/get-random-coordinates.ts
+++ src/utils/get-random-coordinates.ts
@@ -1,7 +1,7 @@
 export const getRandomCoordinates = () => {
   const min = 1;
   const max = 98;
-  const x = Math.floor(Math.random() * (max - min + 1)) + min;
-  const y = Math.floor(Math.random() * (max - min + 1)) + min;
+  const x = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  const y = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
   return [x, y];
 };
