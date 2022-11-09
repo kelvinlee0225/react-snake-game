96c2f66386e8bc5fbe693347e807e522e4472256
kelvinlee0225 <leekelvin0225@gmail.com>
2022-11-09 16:26:26 -0400

    add validations method like checkIfTouchBorder, checkIfCollapse, checkIfEatFood

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import { useState, useEffect } from "react";
 import { GameArena, Container } from "./app-style";
 import { SnakeBody, Food } from "./components";
-import { SnakeDots } from "./components/style";
 import { DIRECTIONS } from "./enums";
+import { getRandomCoordinates } from "./utils";
 
-const getRandomCoordinates = () => {
-  const min = 1;
-  const max = 98;
-  const x = Math.floor(Math.random() * (max - min + 1)) + min;
-  const y = Math.floor(Math.random() * (max - min + 1)) + min;
-  return [x, y];
+const defaultState = {
+  food: getRandomCoordinates(),
+  dots: [
+    [0, 0],
+    [0, 2],
+  ],
+  direction: DIRECTIONS.RIGHT,
+  speed: 200,
 };
 
 function App() {
-  const [food, setFood] = useState(getRandomCoordinates());
-  const [dots, setDots] = useState([
-    [0, 0],
-    [0, 2],
-  ]);
-  const [direction, setDirection] = useState(DIRECTIONS.RIGHT);
-  const [speed, setSpeed] = useState(200);
+  const [food, setFood] = useState(defaultState.food);
+  const [dots, setDots] = useState(defaultState.dots);
+  const [direction, setDirection] = useState(defaultState.direction);
+  const [speed, setSpeed] = useState(defaultState.speed);
 
   const onKeyDown = (e: KeyboardEvent) => {
     switch (e.code) {
@@ -61,12 +60,56 @@ function App() {
     setDots(snakeDots);
   };
 
+  const gameOver = () => {
+    window.alert("GAME OVER!");
+    resetToDefaultState();
+  };
+
+  const increaseSpeed = () => {
+    if (speed > 10) setSpeed((previousSpeed) => previousSpeed - 10);
+  };
+
+  const checkIfTouchBorder = () => {
+    const head = dots[dots.length - 1];
+    if (head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1] < 0)
+      gameOver();
+  };
+
+  const checkIfCollapse = () => {
+    const head = dots[dots.length - 1];
+    const snake = [...dots];
+    snake.pop();
+    snake.forEach((dot) => {
+      if (head[0] === dot[0] && head[1] === dot[1]) gameOver();
+    });
+  };
+
+  const checkIfEatFood = () => {
+    const head = dots[dots.length - 1];
+    const snakeFood = food;
+    if (head[0] === snakeFood[0] && head[1] === snakeFood[1]) {
+      setFood(getRandomCoordinates());
+      dots.unshift([]);
+      increaseSpeed();
+    }
+  };
+
+  const resetToDefaultState = () => {
+    setFood(defaultState.food);
+    setDots(defaultState.dots);
+    setDirection(defaultState.direction);
+    setSpeed(defaultState.speed);
+  };
+
   useEffect(() => {
     document.addEventListener("keydown", onKeyDown);
   }, []);
 
   useEffect(() => {
     const interval = setInterval(moveSnake, speed);
+    checkIfEatFood();
+    checkIfTouchBorder();
+    checkIfCollapse();
     return () => clearInterval(interval);
   });
 
